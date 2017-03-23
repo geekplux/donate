3d43ac3e84510f76db2f95041d407da6d3abde88
geekplux <geekplux@qq.com>
2017-03-23 23:12:10 +0800

    add App.vue

--- App.vue
+++ App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app">
-    <h1>Donate</h1>
+  <div id="app" class="app">
+    <div class="container">
+      <img alt="" src=""/>
+    </div>
   </div>
 </template>
 
 <script>
+
   export default {
   }
 </script>
@@ -13,5 +16,37 @@
   html, body, #app {
     width: 100%;
     height: 100%;
+  }
+  .app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .container {
+    width: 450px;
+    height: 450px;
+    background-color: #eee;
+    box-shadow: 0 2.5rem 5rem rgba(0, 0, 0, .45);
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
+
+
+  @media (max-width: 768px) {
+    .container {
+      width: 320px;
+      height: 320px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .container {
+      width: 250px;
+      height: 250px;
+    }
   }
 </style>
